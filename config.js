7720fa93bb8a89e781cafbca17d0ef55deffb1ff
Hamzaabbas-tech <syedhamzu512@gmail.com>
2024-10-29 11:09:11 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "enter your session",
+SESSION_ID: process.env.SESSION_ID || "xF4B1ZoL#W7z_m961b2gi9Xv-n7pinePHHqRhGdR8_D_7aDZjAKI",
 MONGODB: process.env.MONGODB || "enter mongodb url",
 };
