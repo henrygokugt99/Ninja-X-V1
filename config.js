ea0f96779e80c7542e9998e0c0bf8dc8d8cd8707
Tresh <5liltresh@gmail.com>
2024-12-13 15:32:57 +0100

    Update config.js

--- config.js
+++ config.js
@@ -6,28 +6,23 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-  SESSION_ID:
-    process.env.SESSION_ID ||
-    "2UBGFTzC#pvA8VmI0kFqnRqAFDGu_cGtwvxl2QsJLW45yPVJsE3c",
-  CAPTION: process.env.CAPTION || "POWERED BY TRESH TECH",
-  AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false",
-  READ_MESSAGE: process.env.READ_MESSAGE || "false", // Added auto-read configuration
-  MODE: process.env.MODE || "public",
-  AUTO_VOICE: process.env.AUTO_VOICE || "false",
-  AUTO_STICKER: process.env.AUTO_STICKER || "false",
-  AUTO_REPLY: process.env.AUTO_REPLY || "false",
-  ALIVE_IMG:
-    process.env.ALIVE_IMG || "https://i.postimg.cc/fLT56Lj0/IMG-9750.png",
-  ALIVE_MSG:
-    process.env.ALIVE_MSG ||
-    "HELLO , THIS IS TRESH BOT 😊♻️",
-  ANTI_LINK: process.env.ANTI_LINK || "true",
-  ANTI_BAD: process.env.ANTI_BAD || "true",
-  PREFIX: process.env.PREFIX || ".",
-  FAKE_RECORDING: process.env.FAKE_RECORDING || "FALSE",
-  AUTO_REACT: process.env.AUTO_REACT || "false",
-  HEART_REACT: process.env.HEART_REACT || "false",
-  OWNER_REACT: process.env.OWNER_REACT || "true",
-  BOT_NAME: process.env.BOT_NAME || "TRESH TECH ",
-  OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
+    SESSION_ID: process.env.SESSION_ID || "2UBGFTzC#pvA8VmI0kFqnRqAFDGu_cGtwvxl2QsJLW45yPVJsE3c",
+    CAPTION: process.env.CAPTION || "POWERED BY TRESH TECH",
+    AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+    READ_MESSAGE: process.env.READ_MESSAGE || "true", // Added auto-read configuration
+    MODE: process.env.MODE || "public",
+    AUTO_VOICE: process.env.AUTO_VOICE || "true",
+    AUTO_STICKER: process.env.AUTO_STICKER || "false",
+    AUTO_REPLY: process.env.AUTO_REPLY || "false",
+    ALIVE_IMG: process.env.ALIVE_IMG || "https://i.postimg.cc/fLT56Lj0/IMG-9750.png",
+    ALIVE_MSG: process.env.ALIVE_MSG || "HELLO , THIS IS TRESH BOT 😊♻️",
+    ANTI_LINK: process.env.ANTI_LINK || "true",
+    ANTI_BAD: process.env.ANTI_BAD || "true",
+    PREFIX: process.env.PREFIX || ".",
+    FAKE_RECORDING: process.env.FAKE_RECORDING || "true",
+    AUTO_REACT: process.env.AUTO_REACT || "true",
+    HEART_REACT: process.env.HEART_REACT || "true",
+    OWNER_REACT: process.env.OWNER_REACT || "true",
+    BOT_NAME: process.env.BOT_NAME || "TRESH TECH",
+    OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
 };
